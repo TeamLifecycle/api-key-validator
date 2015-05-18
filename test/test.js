f1c598c067e11142ab4f2f3ac233015ff4848cb7
Tim Giblin <timmyg13@gmail.com>
2015-05-18 14:15:14 -0400

    first test failing

--- test/test.js
+++ test/test.js
@@ -1,14 +1,23 @@
 var nock = require("nock")
+var should = require("chai").should()
 
 describe('when email api keys are valid', function(){
-	it('err should be NULL when key is valid and populated when it is not'), function(){
-		nock.cleanAll()
-		"sendgrid": {
-					"api_user": "sldkfjdslkjf",
-					"api_key": "sldkfjdslkjf"
-				},
-				"mandrill": {
-					"api_key": "sldkfjdslkjf"
-				}
-	})
+	it('result should should be populated and err should be null', function(done){
+		// have service name and api keys
+		// initialize provider client with key
+		// validate keys by calling a function
+		// return callback of (err, result)
+
+		var service = "sendgrid"
+		var apiUser = "ldksafjsd"
+		var apiKey = "ldksafjsd"
+
+		// sendgridClient = new Sendgrid(apiUser, apiKey)
+		sendgridClient.client().validate(function(error, result){
+			should.exist(result);
+			should.not.exist(error);
+			done()
+		})
+
+	});
 });
