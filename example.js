07283576a8d171bb822762b1ba6a588fe5e8ad85
Jake Mooney <jmooney@fandm.edu>
2015-05-27 16:30:13 -0400

    fix syntax error #19

--- example.js
+++ example.js
@@ -3,10 +3,10 @@
 var apiKeyValidator = require('./lib/index');
 
 var keys = {
-    api_user: "key-1f2dj53q21cguk34j1qv1lzee0d-bk70", //enter your own key
-    domain: "email.getvenn.io" //enter your own key
+    auth_id: "MAM2EYYWY0NDK0MWNLN2", //enter your own key
+    auth_token: "YzVmZGIxMGNkODUwMjZjZThhZGE3MjQ1MWM1NmEw" //enter your own key
 }
-apiKeyValidator.validate("mailgun", keys, function(err, result){
+apiKeyValidator.validate("plivo", keys, function(err, result){
     if(err)console.log("invalid!");
     if(result)console.log("valid!");
 })
