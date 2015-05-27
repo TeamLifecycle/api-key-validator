027817fa30d51682437ddbc0ad58e6fc222eaf75
Tim Giblin <timmyg13@gmail.com>
2015-05-27 16:11:56 -0400

    architecture changes

--- example.js
+++ example.js
@@ -1,13 +1,12 @@
 /////////An example of validating keys for twilio///////////
 
-var apiKeyValidator = require('lib/index');
+var apiKeyValidator = require('./lib/index');
 
 var keys = {
-    api_key: "123123", //enter your own api_key
-    auth_token: "2342394" //enter your own auth_token
+    api_user: "key-1f2dj53q21cguk34j1qv1lzee0d-bk70", //enter your own key
+    domain: "email.getvenn.io" //enter your own key
 }
-apiKeyValidator.validate("twilio", keys, function(err, result){
-    console.log(err, result);
+apiKeyValidator.validate("mailgun", keys, function(err, result){
+    if(err)console.log("invalid!");
+    if(result)console.log("valid!");
 })
-
-module.exports = Twilio;
